2672c0846e9f1a3e42bd98c3c0006557cad290d6
W-Divy <divymakwana23@gmail.com>
2026-02-09 17:58:36 +0530

    .

--- application.py
+++ application.py
@@ -1,47 +1,44 @@
-from flask import Flask,request,render_template
-import numpy as np
-import pandas as pd
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
-
+from flask import Flask, request, render_template
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
-app=application
+app = application
+
+# Load model ONCE
+predict_pipeline = PredictPipeline()
+
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route("/predictdata",methods=['GET','POST'])
+
+@app.route("/predictdata", methods=["GET", "POST"])
 def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html')
-    else: 
-        ##predict pipeline class
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score')),
+    try:
+        if request.method == "GET":
+            return render_template("home.html")
 
-
+        data = CustomData(
+            gender=request.form.get("gender"),
+            race_ethnicity=request.form.get("ethnicity"),
+            parental_level_of_education=request.form.get("parental_level_of_education"),
+            lunch=request.form.get("lunch"),
+            test_preparation_course=request.form.get("test_preparation_course"),
+            reading_score=float(request.form.get("reading_score", 0)),
+            writing_score=float(request.form.get("writing_score", 0)),
         )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
 
-        predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
-        return render_template('home.html',results=results[0])
+        pred_df = data.get_data_as_data_frame()
+        results = predict_pipeline.predict(pred_df)
 
-         
-    
+        return render_template("home.html", results=results[0])
+
+    except Exception:
+        import traceback
+        traceback.print_exc()
+        return render_template("home.html", error="Something went wrong during prediction")
 
 
 if __name__ == "__main__":
-    ## while we are going to deploy the project then we dont need debug==True
-    app.run(host='0.0.0.0')
+    app.run()
